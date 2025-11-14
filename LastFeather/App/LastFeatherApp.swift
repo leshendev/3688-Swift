3946a42d29eabb698a8e08e3cdeb4b9092facb8e
SBabchukTM <babchuk.serhii@trident-media.agency>
2025-11-14 14:17:14 +0200

    init

---
+++ LastFeather/App/LastFeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  LastFeatherApp.swift
+//  LastFeather
+//
+//  Created by Serhii Babchuk on 14.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct LastFeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LFLaunchView()
+        }
+    }
+}
---
+++ Pods/Target Support Files/Pods-LastFeather/Pods-LastFeather-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LastFeather : NSObject
+@end
+@implementation PodsDummy_Pods_LastFeather
+@end
